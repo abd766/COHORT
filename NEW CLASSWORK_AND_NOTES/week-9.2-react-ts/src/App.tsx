39f3234d16b72c53f3454c8fa230595fcda2ca45
Abd766 <abdullahmasood766@gmail.com>
2025-02-16 18:48:04 +0530

    Added enums, generics to it

--- NEW CLASSWORK_AND_NOTES/week-9.2-react-ts/src/App.tsx
+++ NEW CLASSWORK_AND_NOTES/week-9.2-react-ts/src/App.tsx
@@ -1,19 +1,16 @@
-import './App.css'
-
+import "./App.css";
 function App() {
-
   return (
     <div>
-      <Todo title='This is todo title' description='this is todo description'/>
+      <Todo title="This is todo title" description="this is todo description" />
     </div>
-  )
+  );
 }
 
-
 // interface are used when we need to implement the structure into a class and you can use nterfaces t aggregate the collection of data
-interface todoProps{
-  title:string,
-  description: string,
+interface todoProps {
+  title: string;
+  description: string;
 }
 
 // Types: we can define multiple types for the same ex types arg = (number | string) this is the property of join in types which is nto available in interfaces
@@ -36,15 +33,38 @@ interface todoProps{
 //   startDate:number,
 // }
 
+// ENums
+// enum Direction {
+//   Up = "up",
+//   Down = "down",
+//   Left = "left",
+//   Right = "right"
+// }
+// function doSomething(keyPressed:Direction){
+//   //do something
+//   if(keyPressed == Direction.Down){
 
-function Todo(props:todoProps){
-  return <div>
-    <h1>
-      {props.title}
-    </h1>
-    <h2>
-      {props.description}
-    </h2>
-  </div>
+//   }
+
+// }
+// doSomething(Direction.Down);
+
+// Generics
+
+// function identity<T>(arr: T){
+//   return arr;
+// }
+// let output1 = identity<string>("mystring");
+// let output2 = identity<number>(1);
+// output1.toLowerCase();
+// console.log(output2);
+
+function Todo(props: todoProps) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+    </div>
+  );
 }
-export default App
+export default App;
